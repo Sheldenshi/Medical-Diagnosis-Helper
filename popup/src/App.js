71472a671c3a11203b43851ad5665a976fc32922
Shelden <shelden@berkeley.edu>
2021-01-07 12:06:12 -0800

    added styles

--- popup/src/App.js
+++ popup/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import React, {useState} from 'react';
 
 
@@ -8,18 +8,17 @@ const App = () => {
     const [diagnoses, setDiagnoses] = useState("")
     return (
         <div className="text-center">
-            <h1 >Tool</h1>
-            <p>
+            <h1>Medical Resources Search Helper</h1>
+            <p className="text-20">
                 Symptoms: {symptoms}
             </p>
             <textarea value = {symptoms} onChange = {event => setSymptoms(event.target.value)}/>
-            <br/>
-            <p>
+            <p className="text-20">
                 Diagnoses: {diagnoses}
             </p>
             <textarea value = {diagnoses} onChange = {event => setDiagnoses(event.target.value)}/>
             <br/>
-            <button className="text-center">Submit</button>
+            <button className="text-15">Search</button>
         </div>
     );
     
