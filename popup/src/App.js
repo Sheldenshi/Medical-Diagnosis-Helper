95490b1a2085b73d974a78313305543b3865cb05
Shelden <shelden@berkeley.edu>
2021-01-07 01:05:45 -0800

    front end no style

--- popup/src/App.js
+++ popup/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import React, {useState} from 'react';
+
+
 
 const App = () => {
+    const [symptoms, setSymptoms] = useState("")
+    const [diagnoses, setDiagnoses] = useState("")
     return (
-        <div>
-            <h1 className="text-center">Test</h1>
+        <div className="text-center">
+            <h1 >Tool</h1>
+            <p>
+                Symptoms: {symptoms}
+            </p>
+            <textarea value = {symptoms} onChange = {event => setSymptoms(event.target.value)}/>
+            <br/>
+            <p>
+                Diagnoses: {diagnoses}
+            </p>
+            <textarea value = {diagnoses} onChange = {event => setDiagnoses(event.target.value)}/>
+            <br/>
+            <button className="text-center">Submit</button>
         </div>
     );
+    
 }
 
 export default App;
