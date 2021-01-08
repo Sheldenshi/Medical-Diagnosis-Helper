7c29e5b7d634ed2608d172415f5d2b8b8f67bed5
Shelden <shelden@berkeley.edu>
2021-01-08 14:05:13 -0800

    added filename: path.resolve(../../templates/index.html) to HtmlWebpackPlugin

--- popup/config/webpack.config.js
+++ popup/config/webpack.config.js
@@ -561,6 +561,7 @@ module.exports = function (webpackEnv) {
           {
             inject: true,
             template: paths.appHtml,
+            filename: path.resolve("../../templates/index.html")
           },
           isEnvProduction
             ? {
