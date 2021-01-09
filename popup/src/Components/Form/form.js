67421c5c3db796620c976c9567bbf9c79c77a764
Shelden <shelden@berkeley.edu>
2021-01-09 13:17:21 -0800

    added form component

---
+++ popup/src/Components/Form/form.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+
+export const From = ({ userInput, onChange }) => {
+    
+    const handleChangeSymptoms = (event) => {
+        onChange({
+            symptoms: event.target.value,
+            diagnoses: userInput.diagnoses
+        })
+    }
+    const handleChangeDiagnoses = (event) => {
+        onChange({
+            symptoms: userInput.symptoms,
+            diagnoses: event.target.value
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+    return (
+        <>
+            <form>
+                <input type='text' required value={userInput.symptoms} onChange={handleChangeSymptoms}></input>
+                <input type='text' required value={userInput.diagnoses} onChange={handleChangeDiagnoses}></input>
+                <input type='submit'></input>
+            </form>
+
+            <form onSubmit={handleSubmit}>
+                <p className="text-15">
+                    Symptoms: {userInput.symptoms}
+                </p>
+                <textarea required value = {userInput.symptoms} onChange = {handleChangeSymptoms}/>
+                <p className="text-15">
+                    Diagnoses: {userInput.diagnoses}
+                </p>
+                <textarea required value = {userInput.diagnoses} onChange = {handleChangeDiagnoses}/>
+                <br/>
+                <Button type="submit" content='Search' />
+            </form>
+        </>
+    )
+}
