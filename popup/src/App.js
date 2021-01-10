49183041ab38255b9653e45a8194a6aedd8c7afb
Shelden <shelden@berkeley.edu>
2021-01-09 16:53:31 -0800

    can send input to main now

--- popup/src/App.js
+++ popup/src/App.js
@@ -10,7 +10,14 @@ const App = () => {
         diagnoses: ''
     })
     const [result, setResult] = useState([])
-
+    const getResult = () => {
+        fetch('/api').then(response => {
+            if(response.ok) {
+                return response.json()
+            }
+        }).then(data => setResult(data))
+    }
+    
     useEffect(() => {
         fetch('/api').then(response => {
             if(response.ok) {
@@ -28,14 +35,25 @@ const App = () => {
             body: JSON.stringify({
                 symptoms: input.symptoms,
                 diagnoses: input.diagnoses
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(response => response.json())
+        .then(message => {
+            console.log(message)
+            setInput({
+                symptoms: '',
+                diagnoses: ''
             })
+            getResult()
         })
     }
 
     return (
         <div className="text-center">
             <h1>Medical Resources Search Helper</h1>
-            <From userInput={input} onChange={handleChange}/>
+            <From userInput={input} onChange={handleChange} handleInputSubmit={handleInputSubmit}/>
             {result.map(result => {
                 return(
                     <ul key={result.id}>
